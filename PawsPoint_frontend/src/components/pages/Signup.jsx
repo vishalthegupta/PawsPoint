e99b89601421236d5ced11f908d374a0022476c5
VedaGit2003 <vedapaulchowdhury@gmail.com>
2024-11-14 01:45:35 +0530

    hot fix

--- PawsPoint_frontend/src/components/pages/Signup.jsx
+++ PawsPoint_frontend/src/components/pages/Signup.jsx
@@ -6,7 +6,7 @@ const Signup = () => {
     <div className='w-full h-full bg-secondary flex flex-col justify-center items-center'>
         <div className='text-gray-300 text-xl font-bold'>Sign Up</div>
         <div className='flex flex-col space-y-1'>
-            <TextInput title={'Email'} placeHolder={'Enter your valid email..'} type={'text'}
+            <TextInput title={'Email'} placeHolder={'Enter your valid email..'} type={'email'}
             theme={'light'}
             />
             <TextInput title={'Password'} placeHolder={'Enter your password...'} type={'password'}
